6a4a3bf91f8a2658953a5da4a4810f6e1020babb
alireza zarei <aliwebto@gmail.com>
2023-07-17 01:42:07 +0330

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,21 @@
 <?php
 
 /*
- * You can place your custom package configuration in here.
+ *
+ * Aliwebto
+ * OTP
+ *
  */
 return [
-
+    "code" => [
+        "type" => env("OTP_CODE_TYPE", "simple"),// full - simple  | full uses all numbers 0-9 but simple uses only 3 numbers and best user experience ,
+        "length" => env("OTP_CODE_LENGTH", 5), // length of code
+        "time" => env("OTP_CODE_TIME", 10) // time of code lifespan in minute
+    ],
+    "security" => [
+        "resendTime" => env("OTP_RESEND_Time", 60) // time of resend code cooldown
+    ],
+    "features" => [
+        "resend" => true // can resend code
+    ]
 ];

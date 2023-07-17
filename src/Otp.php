ef3b8f3daae1509630ea627df975accff2785351
alireza zarei <aliwebto@gmail.com>
2023-07-17 11:26:36 +0330

    check code

--- src/Otp.php
+++ src/Otp.php
@@ -3,6 +3,7 @@
 namespace Aliwebto\Otp;
 
 use Aliwebto\Otp\Models\PhoneCode;
+use Illuminate\Support\Facades\Hash;
 
 class Otp
 {
@@ -11,20 +12,26 @@ class Otp
         self::checkPhoneIsValid($phone);
         $code = self::getCode();
         return PhoneCode::create([
-            "code" => md5($code),
+            "code" => Hash::make($code),
             "phone" => $phone
         ]);
     }
+
+    public static function lastCode($phone): PhoneCode
+    {
+        return PhoneCode::where("phone",$phone)->latest()->first();
+    }
     private static function checkPhoneIsValid($phone)
     {
-        $phoneRegex= "/^(\\+98|0)?9\\d{9}$/";
+        $phoneRegex = "/^(\\+98|0)?9\\d{9}$/";
         throw_if(!preg_match($phoneRegex, $phone), "phone number is wrong");
     }
+
     private static function getDigits(): array
     {
 
         if (config("otp.code.type") == "full") {
-            $digits = [0,1,2,3,4,5,6,7,8,9];
+            $digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         }
 
         if (config("otp.code.type") == "simple") {
@@ -38,6 +45,7 @@ class Otp
         }
         return $digits;
     }
+
     private static function getCode(): string
     {
         $digits = self::getDigits();
@@ -46,19 +54,59 @@ class Otp
             return "Invalid input";
         }
         shuffle($digits);
-        $number =  implode("", array_slice($digits, 0, $length));
+        $number = implode("", array_slice($digits, 0, $length));
         while (strlen($number) < 5) {
             shuffle($digits);
-            $number .= implode("", array_slice($digits, 0, $length-strlen($number)));
+            $number .= implode("", array_slice($digits, 0, $length - strlen($number)));
         }
-        return  $number;
+        return $number;
     }
 
-    public static function regenerateCode($phone)
+    public static function regenerate($phone)
     {
         throw_if(config("otp.features.resend") == false, "Resend feature isn't active");
         self::checkPhoneIsValid($phone);
+        $lastCode = self::lastCode($phone);
+        if ($lastCode){
+            throw_if(self::checkRegenerateCooldown($lastCode),"Regenerate Cooldown");
+        }
         PhoneCode::where("phone", $phone)->delete();
         return self::generate($phone);
+    }
+
+    public static function checkRegenerateCooldown(PhoneCode $code): bool
+    {
+        $cooldown = config("otp.security.resendTime");
+        return self::regenerateCooldown($code) !== 0;
+    }
+
+    public static function regenerateCooldown(PhoneCode $code): int
+    {
+        $cooldown = config("otp.security.resendTime");
+        return ($code->created_at->addSeconds(intval($cooldown)) < now()) ? 0 : now()->diffInRealSeconds($code->created_at->addSeconds(intval($cooldown)));
+    }
+
+    public static function checkLifeSpan(PhoneCode $code): bool
+    {
+        $lifespan = config("otp.code.time");
+        return $code->created_at > now()->subMinutes(intval($lifespan));
+    }
+
+    public static function checkValid($code, $phone): bool
+    {
+        $phone_code = PhoneCode::where("phone",$phone)->first();
+        if (!is_null($phone_code)){
+            return Hash::check($code,$phone_code->code);
+        }
+        return false;
+    }
+
+    public static function check($code, $phone): bool
+    {
+        $phone_code = PhoneCode::where("phone",$phone)->first();
+        if (!is_null($phone_code)){
+            return self::checkValid($code,$phone) and self::checkLifeSpan($phone_code);
+        }
+        return false;
     }
 }
